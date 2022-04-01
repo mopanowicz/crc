7564747730d327ec0665ea8fa9262f1d9b0372b7
mopanowicz <marek.opanowicz@gmail.com>
2022-04-01 20:05:35 +0200

    Add home controller and template

---
+++ examples/demo-api/src/main/java/com/example/demoapi/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.example.demoapi.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+  final static Logger log = LoggerFactory.getLogger(HomeController.class);
+  
+  final static String NA = "n/a";
+
+  @GetMapping("/")
+  public ModelAndView home() throws IOException {
+    ModelAndView model = new ModelAndView("home");
+    Properties props = readManifest();
+    addObject(model, "buildJdk", props.getProperty("Build-Jdk-Spec"), NA);
+    addObject(model, "createdBy", props.getProperty("Created-By"), NA);
+    addObject(model, "javaVersion", System.getProperties().getProperty("java.version"), NA);
+    addObject(model, "hostName", System.getenv("HOSTNAME"), NA);
+    return model;
+  }
+
+  void addObject(ModelAndView target, String objectName, String objectValue, String defaultValue) {
+    target.addObject(objectName, StringUtils.hasText(objectValue) ? objectValue : defaultValue);
+  }
+
+  Properties readManifest() throws IOException {
+    InputStream is = this.getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+    Properties properties = new Properties();
+    properties.load(is);
+    return properties;
+  }
+}
