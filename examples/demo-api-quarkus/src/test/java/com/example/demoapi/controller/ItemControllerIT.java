7f64a303c0f4e0f1a81823703306d79e173eb1b7
mopanowicz <marek.opanowicz@gmail.com>
2022-04-30 17:33:11 +0200

    Add integration tests

---
+++ examples/demo-api-quarkus/src/test/java/com/example/demoapi/controller/ItemControllerIT.java
@@ -0,0 +1,8 @@
+package com.example.demoapi.controller;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ItemControllerIT extends ItemControllerTest {
+  // Execute the same tests but in native mode.
+}
---
+++ examples/demo-api-quarkus/src/test/java/com/example/demoapi/service/ItemServiceIT.java
@@ -0,0 +1,8 @@
+package com.example.demoapi.service;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ItemServiceIT extends ItemServiceTest {
+  // Execute the same tests but in native mode.
+}
