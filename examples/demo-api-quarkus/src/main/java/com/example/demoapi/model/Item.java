40f35e7c58ae5a7d8b4ece97ac88b16733ebffb1
mopanowicz <marek.opanowicz@gmail.com>
2022-04-30 17:23:26 +0200

    Add constraint

--- examples/demo-api-quarkus/src/main/java/com/example/demoapi/model/Item.java
+++ examples/demo-api-quarkus/src/main/java/com/example/demoapi/model/Item.java
@@ -4,9 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 
 @Entity
+@Table(
+    name = "ITEM",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 public class Item {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
