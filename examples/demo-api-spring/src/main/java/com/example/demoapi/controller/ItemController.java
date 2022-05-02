bc6de0109779309b9d66efe9a314de6fa6f7f0b1
mopanowicz <marek.opanowicz@gmail.com>
2022-05-02 15:19:45 +0200

    Add 409

--- examples/demo-api-spring/src/main/java/com/example/demoapi/controller/ItemController.java
+++ examples/demo-api-spring/src/main/java/com/example/demoapi/controller/ItemController.java
@@ -2,13 +2,23 @@ package com.example.demoapi.controller;
 
 import javax.persistence.NoResultException;
 
-import com.example.demoapi.dto.ItemDTO;
-import com.example.demoapi.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demoapi.dto.ItemDTO;
+import com.example.demoapi.service.ItemService;
 
 @RestController
 @CrossOrigin
@@ -27,6 +37,9 @@ public class ItemController {
   public ResponseEntity<ItemDTO> create(@RequestBody ItemDTO dto) {
     try {
       return new ResponseEntity<>(itemService.create(dto), HttpStatus.CREATED);
+    } catch (DataIntegrityViolationException e) {
+      log.error("create failed", e);
+      return new ResponseEntity<>(HttpStatus.CONFLICT);
     } catch (Exception e) {
       log.error("create failed", e);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
