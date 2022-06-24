0dfc059460eddea5f79d5dfdc243df13dba9af77
mopanowicz <marek.opanowicz@gmail.com>
2022-06-24 20:49:27 +0200

    Add generated project

---
+++ examples/event-feed/event-feed-mongo/src/main/java/com/example/event/EventFeedMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFeedMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFeedMongoApplication.class, args);
+	}
+
+}
