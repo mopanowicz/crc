54ac45ad79d839294602f4f6b214c1ac1b2baeb4
mopanowicz <marek.opanowicz@gmail.com>
2024-01-07 14:53:59 +0100

    Regenerate demo-ng

--- examples/demo-ng/src/app/config.ts
+++
@@ -1,3 +0,0 @@
-export class Config {
-  static apiRootUrl: string;
-}
--- examples/demo-ng/src/main.ts
+++ examples/demo-ng/src/main.ts
@@ -1,22 +1,6 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-import { Config } from './app/config';
-import { KeycloakService } from './app/keycloak.service';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-fetch('assets/config.json')
-  .then(response => response.json())
-  .then(json => {
-    Config.apiRootUrl = json.apiRootUrl;
-
-    KeycloakService.init()
-      .then(() => {
-        platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
-      })
-  });
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
