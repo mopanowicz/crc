099f7f9829bf5c29b977cb935720bdbc38a3bfec
mopanowicz <marek.opanowicz@gmail.com>
2025-07-31 14:35:10 +0200

    Rename projects and upgrade spring boot + remove mongodb

---
+++ examples/spring-kafka-logback/src/test/java/com/example/poc/SpringKafkaLogbackApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:19092", "port=19092"})
+class SpringKafkaLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
