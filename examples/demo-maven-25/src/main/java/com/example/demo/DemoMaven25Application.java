4b9c0cfc2ff4f3c1ea8d0086528301abde516dbd
mopanowicz <marek.opanowicz@gmail.com>
2025-09-20 20:22:53 +0200

    Rename and regenerate demo projects + fix gradle project

---
+++ examples/demo-maven-25/src/main/java/com/example/demo/DemoMaven25Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMaven25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMaven25Application.class, args);
+	}
+
+}
