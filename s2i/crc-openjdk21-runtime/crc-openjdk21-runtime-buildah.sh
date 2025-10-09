7975e086aa806da21df511bc248e0e7b23a58337
Marek Opanowicz <marek.opanowicz@gmail.com>
2025-10-09 13:35:04 +0200

    Build crc-openjdk21-runtime with buildah

---
+++ s2i/crc-openjdk21-runtime/crc-openjdk21-runtime-buildah.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# run within buildah unsharez   
+
+set -e
+
+microcontainer=$(buildah from localhost/micro/crc-ubi8)
+micromount=$(buildah mount $microcontainer)
+
+_JAVA_VERSION=21
+_JAVA_HOME=/usr/lib/jvm/jre
+
+buildah config --port 8080 --port 8443 ${microcontainer}
+
+buildah config --label io.openshift.expose-services="8080:http,8443:https" ${microcontainer}
+
+buildah config --env JAVA_VERSION=${_JAVA_VERSION} --env JAVA_HOME=${_JAVA_HOME} ${microcontainer}
+
+buildah copy ${microcontainer} ./root/ /
+
+dnf -y install --installroot ${micromount} --releasever 8 --nogpgcheck --setopt=tsflags=nodocs --setopt=install_weak_deps=False \
+    java-${_JAVA_VERSION}-openjdk-headless
+
+dnf -y clean all --installroot ${micromount} --releasever 8 --nogpgcheck
+
+buildah umount $microcontainer
+buildah commit $microcontainer micro/crc-openjdk21-runtime
---
+++ s2i/crc-ubi8/crc-ubi8-buildah.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# run within buildah unsharez   
+
+set -e
+
+microcontainer=$(buildah from rockylinux/rockylinux:8-ubi-micro)
+micromount=$(buildah mount $microcontainer)
+
+_VERSION_TAG=${_VERSION_TAG:-latest}
+_HOME=/app
+
+buildah config --env HOME=${_HOME} --env VERSION_TAG=${_VERSION_TAG} ${microcontainer}
+
+buildah copy ${microcontainer} ./root/ /
+
+dnf -y update --installroot ${micromount} --releasever 8 --nogpgcheck --setopt=tsflags=nodocs --setopt=install_weak_deps=False
+dnf -y install --installroot ${micromount} --releasever 8 --nogpgcheck --setopt=tsflags=nodocs --setopt=install_weak_deps=False \
+    curl \
+    findutils \
+    glibc-langpack-en \
+    glibc-langpack-pl \
+    gzip \
+    iputils \
+    openssh-clients \
+    openssl \
+    procps \
+    tar \
+    which \
+    zip
+
+dnf -y install --installroot ${micromount} --releasever 8 --nogpgcheck --setopt=tsflags=nodocs --setopt=install_weak_deps=False \
+    shadow-utils \
+
+buildah run ${microcontainer} -- groupadd -g 1001 default
+buildah run ${microcontainer} -- useradd -u 1001 -g 1001 -d ${_HOME} -m -s /sbin/nologin default
+buildah run ${microcontainer} -- chmod a+rwx ${_HOME}
+
+dnf -y remove --installroot ${micromount} --releasever 8 --nogpgcheck shadow-utils
+dnf -y clean all --installroot ${micromount} --releasever 8 --nogpgcheck
+
+buildah config --user 1001 ${microcontainer}
+
+buildah config --workingdir ${_HOME} ${microcontainer}
+
+buildah config --entrypoint container-entrypoint ${microcontainer}
+
+buildah umount $microcontainer
+buildah commit $microcontainer micro/crc-ubi8
