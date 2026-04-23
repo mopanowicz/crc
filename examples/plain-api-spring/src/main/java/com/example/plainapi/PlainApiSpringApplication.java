ed9a0fcd193019215ad5f3a2540b83fccc81aa9a
mopanowicz <marek.opanowicz@gmail.com>
2026-04-23 10:45:51 +0200

    Add plain-api-spring

---
+++ examples/plain-api-spring/src/main/java/com/example/plainapi/PlainApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.plainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlainApiSpringApplication.class, args);
+	}
+
+}
---
+++ examples/plain-api-spring/src/test/java/com/example/plainapi/PlainApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
